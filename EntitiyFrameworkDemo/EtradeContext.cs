708e2576f0c1906142f24a2d30d5b3843c276e41
Zehra Asel <zehra.asel@gmail.com>
2025-07-23 18:36:12 +0300

    Yeni projeler eklendi

---
+++ EntitiyFrameworkDemo/EtradeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyFrameworkDemoü
+{
+    class EtradeContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }//Veilen isimde tablo olacak ona göre çalışır
+
+    }
+}
