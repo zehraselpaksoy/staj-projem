87b89e19cb9606604cbce161e0622b328e866ad3
Zehra Asel <zehra.asel@gmail.com>
2025-07-23 18:42:47 +0300

    RecapProject1 klasörü yeniden eklendi

---
+++ RecapProject1/RecapProject1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapProject1
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListProduct();
+            ListCategory();
+        }
+
+        private void ListProduct()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.ToList();
+            }
+        }
+        private void ListProductByCategory(int categoryId)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p => p.CategoryId==categoryId).ToList();
+            }
+        }
+        private void ListProductByProductName(string key)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p => p.ProductName.ToLower().Contains(key.ToLower())).ToList();
+            }
+        }
+        private void ListCategory()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                cbxCategory.DataSource = context.Categories.ToList();
+                cbxCategory.DisplayMember = "CategoryName";//görülecek değer
+                cbxCategory.ValueMember = "CategoryId";//Göstrecek değiri ismi al ama seçtiğimde değer idsi olsun
+                
+            }
+        }
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ListProductByCategory(Convert.ToInt32(cbxCategory.SelectedValue));
+
+            }
+            catch 
+            {
+
+            }
+        }
+
+        private void tbxSearch_TextChanged(object sender, EventArgs e)
+        {
+            string key = tbxSearch.Text;
+            if (string.IsNullOrEmpty(key))
+            {
+                ListProduct();
+            }
+            else
+            {
+                ListProductByProductName(tbxSearch.Text);
+            }
+               
+        }
+    }
+}
